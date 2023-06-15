e68dfd35af769dd1975ddbee14ff5e53ae710ba5
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2023-06-15 13:48:54 +0300

    Some essential codes for grabbing system

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	FRotator MyRotation = GetComponentRotation();
+	FString RotationString = MyRotation.ToCompactString();
+	UE_LOG(LogTemp, Display, TEXT("Grabber Rotation: %s"), *RotationString)
+	float Time = GetWorld()->TimeSeconds;
+	UE_LOG(LogTemp,Display,TEXT("Current time is: %f"), Time);
+	// ...
+}
--- Source/CryptRaider/Grabber.h
+++ Source/CryptRaider/Grabber.h
@@ -3,18 +3,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
-#include "Mover.generated.h"
+#include "Components/SceneComponent.h"
+#include "Grabber.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class CRYPTRAIDER_API UMover : public UActorComponent
+class CRYPTRAIDER_API UGrabber : public USceneComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UMover();
+	UGrabber();
 
 protected:
 	// Called when the game starts
@@ -23,15 +23,6 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-private:
-	UPROPERTY(EditAnywhere)
-	FVector MoveOffSet;
-	
-	UPROPERTY(EditAnywhere)
-	float MoveTime = 4.f;
-	
-	UPROPERTY(EditAnywhere)
-	bool ShouldMove = false;
-	
-	FVector OriginalLocation;
+
+		
 };
